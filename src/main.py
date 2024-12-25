8479b8d041585aa9691d8954b0b380b25402449c
buithanhtien01111992 <buithanhtien.spkt@gmail.com>
2024-12-25 16:44:21 +0700

    fix buildspec

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.title("Hello World from CloudMentorPro")
-st.title(" deloy ecs blue/green ")
+st.title("Hello World from TienBT")
+st.title(" deloy ecs blue/green v2 ")
 # st.title(" test cicd code pipeline ")
